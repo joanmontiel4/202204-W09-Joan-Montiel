3064a5eeaebc16f4c8df97dc8cf5e888100dd08b
joanmontiel4 <joanmontiel@gmail.com>
2022-06-06 11:51:45 +0200

    Modify app.js to include addtional webpages

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,9 +1,23 @@
 import { Header } from './components/header.js';
+import { Footer } from './components/footer.js';
 import { Main } from './components/main.js';
 (() => {
     document.addEventListener('DOMContentLoaded', () => {
-        new Header('slot.header');
-        new Main('slot.main-sec');
+        const path = location.pathname.split('/');
+        if (
+            path[path.length - 1] === '' ||
+            path[path.length - 1] === 'index.html'
+        ) {
+            commonComponents();
+        } else if (path[path.length - 1] === 'mypokemons.html') {
+            commonComponents();
+        } else if (path[path.length - 1] === 'details.html') {
+            commonComponents();
+        }
     });
 })();
-//# sourceMappingURL=app.js.map
+
+function commonComponents() {
+    new Header('slot.header');
+    new Footer('slot.footer');
+}
